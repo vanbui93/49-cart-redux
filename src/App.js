d5bbe81e5538a56da910cb26356a9d46987fe6ae
Van Bui <van.bt@trans-cosmos.com.vn>
2020-05-11 13:16:35 +0700

    first initialization

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Mesage from './components/Mesage';
+import Cart from './components/Cart';
+import Header from './components/Header';
+import Product from './components/Product';
+import Footer from './components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App hidden-sn animated deep-purple-skin">
+        <div>
+          <Header />
+          <main id="mainContainer">
+            <div className="container">
+              <Product />
+              <Mesage />
+              <Cart />
+            </div>
+          </main>
+          <Footer />
+        </div>
+      </div>
+    );
+  }
+
 }
-
 export default App;
